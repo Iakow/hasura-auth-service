3433767c43c092e8760c853e4098b6ea015cc396
Yakov Ponomarenko <ponomarykov@gmail.com>
2022-10-25 15:01:50 +0300

    added cors

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 import userController from "./controllers/user.controller.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
